e4c90544aba4b5650d83b923f8cbe78e15756add
jkMugabe <jmugabe029@gmail.com>
2019-01-17 15:51:25 +0300

    updated views.py on develop branch

--- APi/views.py
+++ APi/views.py
@@ -33,28 +33,60 @@ def create_redflag():
         "comment": request.json.get("comment")
     }
     redflags.append(redflag)
-    return jsonify({'redflags': redflag, 'status': 201}), 201
+    return jsonify({'data': redflag, 'status': 201}), 201
 
 
-<<<<<<< HEAD
-@app.route('/api/v1/DELETE/redflags/<int:redflag_id>', methods=['DELETE'])
-def delete_redflag(redflag_id):
+@app.route('/api/v1/GET/redflags', methods=['GET'])
+def get_redflags():
+    if len(redflags) == 0:
+        return jsonify({'status': 404,
+                        'error': 'No redflag available'}), 404
+    return jsonify({'data': redflags,
+                    'status': 200}), 200
+
+
+@app.route('/api/v1/GET/redflags/<int:redflag_id>', methods=['GET'])
+def get_redflag(redflag_id):
     redflag = [redflag for redflag in redflags if redflag['id'] == redflag_id]
     if len(redflag) == 0:
+
         return jsonify(
             {
                 'status': 404,
                 'error': 'Redflag with that ID is not available'
             }), 404
-    redflags.remove(redflag[0])
-    redfla = [{'message': 'red-flag record has been deleted',
-               'id': redflag[0]['id'], 'redflag': redflag[0]}]
-    return jsonify({
-        'status': 206,
-        'data': redfla,
+    return jsonify({'status': 200, 'data': redflag[0]}), 200
 
-    }), 206
-=======
+
+@app.route('/api/v1/PUT/redflags/<int:redflag_id>/edit_location', methods=['PUT'])
+def update_location(redflag_id):
+    redflag = [redflag for redflag in redflags if redflag['id'] == redflag_id]
+    if len(redflag) == 0:
+        return jsonify({'status': 404,
+                        'error': 'No redflag available'}), 404
+    if not request.json:
+        return jsonify(
+            {
+                'status': 400,
+                'error': 'Use the required format'
+            }), 400
+
+    if 'location' in request.json and type(request.json['location']) is not str:
+        return jsonify(
+            {
+                'status': 400,
+                'error': 'location should be a string'
+            }), 400
+
+    redflag[0]['location'] = request.json.get('location', redflag[0]['location'])
+    redflags.append(redflag[0])
+    return jsonify({
+        'task': redflag[0],
+        'status': 210,
+        'id': redflag[0]['id'],
+        'message': 'Updated red-flag record\'s location'}), 210
+
+
 @app.route('/api/v1/PUT/redflags/<int:redflag_id>/edit_comment', methods=['PUT'])
 def update_comment(redflag_id):
     redflag = [redflag for redflag in redflags if redflag['id'] == redflag_id]
@@ -78,12 +110,30 @@ def update_comment(redflag_id):
 
     redflag[0]['comment'] = request.json.get('comment', redflag[0]['comment'])
     redflags.append(redflag[0])
+    data1 = [{'redflag': redflag[0], 'message': 'Updated red-flag record\'s comment'}]
     return jsonify({
-        'task': redflag[0],
+        'data': data1,
         'status': 210,
-        'id': redflag[0]['id'],
-        'message': 'Updated red-flag record\'s comment'}), 210
->>>>>>> cf6033f7c4016f30eee1a00976c26d8cb5c867bb
+    }), 210
+
+
+@app.route('/api/v1/DELETE/redflags/<int:redflag_id>', methods=['DELETE'])
+def delete_redflag(redflag_id):
+    redflag = [redflag for redflag in redflags if redflag['id'] == redflag_id]
+    if len(redflag) == 0:
+        return jsonify(
+            {
+                'status': 404,
+                'error': 'Redflag with that ID is not available'
+            }), 404
+    redflags.remove(redflag[0])
+    redfla = [{'message': 'red-flag record has been deleted',
+               'id': redflag[0]['id'], 'redflag': redflag[0]}]
+    return jsonify({
+        'status': 206,
+        'data': redfla,
+
+    }), 206
 
 
 if __name__ == '__main__':
